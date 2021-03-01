12afb20192da7cddcc7dc6678d4a6284545cb312
seraph <seraph@mindmini.com>
2021-03-02 01:34:32 +0800

    计算字符数目

---
+++ 111801206/src/WordsProcess.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class WordsProcess {
+    private int numberChar;
+    private int numberLine;
+    private int numberWord;
+    private HashMap<String,Integer> map;
+    private String inputName;
+    private String outputName;
+    private FileReader fileReader;
+
+    WordsProcess(String input,String output){
+        numberChar = 0;
+        numberLine = 0;
+        numberWord = 0;
+        map = new HashMap<>();
+        inputName = input;
+        outputName = output;
+    }
+
+    //统计字符数
+    private void countChar(){
+        try {
+            int ch;
+            BufferedReader br = new BufferedReader(new FileReader(inputName));
+            while((ch = br.read()) != -1){
+                if(ch < 128)numberChar++;
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
