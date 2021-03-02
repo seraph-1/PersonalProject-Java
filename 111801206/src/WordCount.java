2c3f08db2f6602b6d3865f06f8da3947aad943e8
seraph <seraph@mindmini.com>
2021-03-02 23:54:16 +0800

    可以在cmd上运行了

--- 111801206/src/WordCount.java
+++ 111801206/src/WordCount.java
@@ -1,7 +1,13 @@
-package src;
 
 public class WordCount {
-    public static void main(String[] args){
-
-    }
+        public static void main(String[] args){
+        if(args.length != 2){
+            System.out.println("there are not 2 parameters");
+            return;
+        }
+        String input = args[0];
+        String output = args[1];
+            WordsProcess wp = new WordsProcess(input,output);
+            wp.deal();
+        }
 }
